4c86432f681dced6d7729de4bbab3164eccf472e
Gabriel Bordin <Bordin307k@gmail.com>
2022-05-09 14:32:41 -0300

    Aula 3

--- Cards/CardController.cs
+++ Cards/CardController.cs
@@ -7,6 +7,8 @@ namespace Cards
 {
     public class CardController : MonoBehaviour
     {
+        public float cardYOffset;
+
         private void Start()
         {
             GenerateHand(GlobalPlayer.instance.deck);
@@ -15,14 +17,34 @@ namespace Cards
         private void GenerateHand(List<Card> instanceDeck)
         {
             GameObject cardPrefab = GlobalCards.instance.cardPrefab; // Pega o prefab da carta
+            List<GameObject> allGameObjectCards = new List<GameObject>();
             foreach (Card card in instanceDeck) //Para cada carta no deck do jogador
             {
                 GameObject newCard = Instantiate(cardPrefab, GlobalCards.instance.cardCanvas.transform); //Instancia a carta
+                allGameObjectCards.Add(newCard);
                 CardElements element = newCard.GetComponent<CardElements>(); //Pega o script com os elementos separados da carta
                 //Aplica as informações da carta nos elementos
                 element.cost.text = card.cost.ToString();
                 element.image.sprite = card.image;
                 element.text.text = GenerateDescription(card.effects);
+            }
+
+            AttCardsPosition(allGameObjectCards);
+        }
+
+        private void AttCardsPosition(List<GameObject> allGameObjectCards)
+        {
+            //Vector2 screenSize = new Vector2(Screen.width, Screen.height); Vamos utilizar para criar um "respiro"
+            int cardIdx = 0;
+            foreach (var cardGO in allGameObjectCards)
+            {
+                RectTransform cardTransform = cardGO.GetComponent<RectTransform>(); //Pega o rect transform do objeto da carta
+                Vector2 cardSize = cardTransform.anchorMax - cardTransform.anchorMin; //Adquire o tamnho da carta
+                cardTransform.anchorMin = new Vector2(cardSize.x * cardIdx, 0); //Posiciona o x minimo da ancora no tamanho da carta * o index dela
+                cardTransform.anchorMax = cardTransform.anchorMin + cardSize;  //Com base na posição minima, adiciona o tamanho da carta para gerar a posição máxima da ancora
+                cardTransform.offsetMax = new Vector2(0, -cardYOffset); //Ajusta os valores do tamanho da imagem em pixel para ficar perto das ancoras
+                cardTransform.offsetMin = new Vector2(0, -cardYOffset);
+                cardIdx++;
             }
         }
 
--- Cards/CardElements.cs
+++ Cards/CardElements.cs
@@ -1,7 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Playables;
 using UnityEngine.UI;
 
 public class CardElements : MonoBehaviour
@@ -9,4 +12,26 @@ public class CardElements : MonoBehaviour
     public Image image;
     public TextMeshProUGUI text;
     public TextMeshProUGUI cost;
+
+    public PlayableAsset openAnim;
+    public PlayableAsset closeAnim;
+    
+    private PlayableDirector director;
+
+    private void Awake()
+    {
+        director = GetComponent<PlayableDirector>();
+    }
+
+    public void OnMouseEnter()
+    {
+        director.playableAsset = openAnim;
+        director.Play();
+    }
+
+    public void OnMouseExit()
+    {
+        director.playableAsset = closeAnim;
+        director.Play();
+    }
 }
